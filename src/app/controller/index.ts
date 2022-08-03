7ff4fa9444a770d228be64b9a577eeb32379a56b
sneha <snehasankar24@gmail.com>
2022-08-03 18:03:04 +0530

    deleted addresscontroller

--- src/app/controller/index.ts
+++ src/app/controller/index.ts
@@ -1,13 +1,10 @@
 /**
  * Wraps Controllers for easy import from other modules
  */
-import { AddressRepository } from "../repositories/AddressRepository";
 import { DepartmentRepository } from "../repositories/DepartmentRepository";
 import { EmployeeRepository } from "../repositories/EmployeeRepository";
-import { AddressService } from "../service/AddressService";
 import { DepartmentService } from "../service/DepartmentService";
 import { EmployeeService } from "../service/EmployeeService";
-import AddressController from "./AddressController";
 import DepartmentController from "./DepartmentController";
 import EmployeeController from "./EmployeeController";
 import HealthController from "./HealthController";
@@ -15,5 +12,4 @@ export default [
   new HealthController(),
   new EmployeeController(new EmployeeService( new EmployeeRepository())),
   new DepartmentController(new DepartmentService(new DepartmentRepository())),
-  new AddressController(new AddressService(new AddressRepository()))
 ];
